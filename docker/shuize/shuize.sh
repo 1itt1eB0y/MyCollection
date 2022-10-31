5079607bbb06b931384094580df6ada434b10a56
1itt1eB0y <29831189+1itt1eB0y@users.noreply.github.com>
2022-10-31 12:59:53 +0800

    Update shuize.sh

--- docker/shuize/shuize.sh
+++ docker/shuize/shuize.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -it -v $(pwd)/iniFile:/ShuiZe_0x727/iniFile -v $(pwd)/result:/ShuiZe_0x727/result --name shuize shuize:latest $1
+docker run -it -v $(pwd)/iniFile:/ShuiZe_0x727/iniFile -v $(pwd)/result:/ShuiZe_0x727/result --name shuize 1itt1eb0y/shuize:latest $1
